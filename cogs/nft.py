65b4b829a2dbefaf424d4c0874d7312f0377041d
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-02-04 16:52:49 +0800

    Actually fixed it

--- cogs/nft.py
+++ cogs/nft.py
@@ -2,6 +2,8 @@ from discord.ext import commands
 import discord
 import aiohttp
 import random
+import os.path
+import io
 
 
 class Nft(commands.Cog):
@@ -11,18 +13,18 @@ class Nft(commands.Cog):
 
     @commands.command()
     async def nft(self, ctx):
-        with open('../assets/nft_list.txt', 'r') as f:
+        with open(os.path.dirname(__file__) + '/../assets/nft_list.txt',
+                  'r') as f:
             urls = f.read().split('\n')
             url = random.choice(urls)
-        
+
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 img = await resp.read()
-                with io.BytesIO(img) as file: 
-                    await ctx.send(file=discord.File(file, "generated_nft.png"))
-
+                with io.BytesIO(img) as file:
+                    await ctx.send(file=discord.File(file, 'generated_nft.png')
+                                   )
 
 
 async def setup(bot):
     await bot.add_cog(Nft(bot))
-
