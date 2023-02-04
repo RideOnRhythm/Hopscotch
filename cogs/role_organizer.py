a3a3b27672ae3783794655e752d7f78d10bde8cf
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-02-04 13:34:59 +0800

    Fixed infinite loop of bot adding and removing the exclusive role

--- cogs/role_organizer.py
+++ cogs/role_organizer.py
@@ -38,14 +38,14 @@ class RoleOrganizer(commands.Cog):
     async def on_member_update(self, before, after):
         if before.roles == after.roles or after.bot:
             return
-        await self.update_roles(after)
         exclusive_roles = [1064445950992597042, 1052103824346710056, 858166817145880587, 957799704660439041, 1044612642599358475, 957803140864950302, 858222265098174474, 858228707767877632, 1026064526698889286, 1067326246867980318, 1036568309501526026, 1036842427622903878, 1063688787722522666, 1055792475245129759, 1058290811835531304, 1045704861783695450]
         added_role = next((role for role in after.roles if role not in before.roles), None)
         hops_dev = after.guild.get_role(934678477825802270)
         if added_role is not None:
             if added_role.id in exclusive_roles:
-                entries = [entry async for entry in after.guild.audit_logs(limit=10, action=discord.AuditLogAction.member_role_update)]
-                role_entry = next((entry for entry in entries if hops_dev in entry.user.roles and entry.target == after), None)
+                entries = [entry async for entry in after.guild.audit_logs(limit=1, action=discord.AuditLogAction.member_role_update)]
+                role_entry = next((entry for entry in entries if (hops_dev in entry.user.roles or entry.user.bot)
+                     and entry.target == after), None)
                 if not role_entry:
                     await after.remove_roles(added_role)
         else:
@@ -53,10 +53,12 @@ class RoleOrganizer(commands.Cog):
             if removed_role is None:
                 return
             if removed_role.id in exclusive_roles:
-                entries = [entry async for entry in after.guild.audit_logs(limit=10, action=discord.AuditLogAction.member_role_update)]
-                role_entry = next((entry for entry in entries if hops_dev in entry.user.roles and entry.target == after), None)
+                entries = [entry async for entry in after.guild.audit_logs(limit=1, action=discord.AuditLogAction.member_role_update)]
+                role_entry = next((entry for entry in entries if (hops_dev in entry.user.roles or entry.user.bot)
+                     and entry.target == after), None)
                 if not role_entry:
                     await after.add_roles(removed_role)
+        await self.update_roles(after)
 
 
 
