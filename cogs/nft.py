d697e360f452b67366226be44e62b1512c70b240
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-02-04 16:47:02 +0800

    Added NFT generator

--- cogs/nft.py
+++ cogs/nft.py
@@ -1,1 +1,28 @@
+from discord.ext import commands
+import discord
+import aiohttp
+import random
+
+
+class Nft(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command
+    async def nft(self, ctx):
+        with open('../assets/nft_list.txt', 'r') as f:
+            urls = f.read().split('\n')
+            url = random.choice(urls)
+        
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                img = await resp.read()
+                with io.BytesIO(img) as file: 
+                    await channel.send(file=discord.File(file, "generated_nft.png"))
+
+
+
+async def setup(bot):
+    await bot.add_cog(Nft(bot))
 
