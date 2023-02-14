7c722d4cc5dc9b8ee6d57d4b78dc49af24640236
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-02-14 13:25:25 +0800

    nft

--- cogs/nft.py
+++ cogs/nft.py
@@ -12,18 +12,21 @@ class Nft(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def nft(self, ctx):
-        with open(os.path.dirname(__file__) + '/../assets/nft_list.txt',
-                  'r') as f:
-            urls = f.read().split('\n')
-            url = random.choice(urls)
+    async def nft(self, ctx, amount=1):
+        files = []
+        for i in range(amount):
+            with open(
+                    os.path.dirname(__file__) + '/../assets/nft_list.txt',
+                    'r') as f:
+                urls = f.read().split('\n')
+                url = random.choice(urls)
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                img = await resp.read()
-                with io.BytesIO(img) as file:
-                    await ctx.send(file=discord.File(file, 'generated_nft.png')
-                                   )
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    img = await resp.read()
+                    with io.BytesIO(img) as file:
+                        files.append(discord.File(file, 'generated_nft.png'))
+        await ctx.send(files=files)
     
     @commands.command()
     async def add_nft(self, ctx, image: typing.Union[str, discord.Attachment]):
