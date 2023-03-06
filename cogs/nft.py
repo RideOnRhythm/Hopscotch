9c0cfee784192f9fe0fcc7ec373e605e3b2ac8e7
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-03-06 08:24:21 +0800

    100 no repeating

--- cogs/nft.py
+++ cogs/nft.py
@@ -11,6 +11,7 @@ class Nft(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.queue = []
 
     @commands.command()
     async def nft(self, ctx, amount=1):
@@ -21,7 +22,15 @@ class Nft(commands.Cog):
                     'r') as f:
                 urls = f.read().split('\n')
                 url = random.choice(urls)
-
+            while url in self.queue:
+                with open(
+                        os.path.dirname(__file__) + '/../assets/nft_list.txt',
+                        'r') as f:
+                    urls = f.read().split('\n')
+                    url = random.choice(urls)
+            self.queue.append(url)
+            if len(self.queue) >= 100:
+                self.queue = []
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as resp:
                     img = await resp.read()
