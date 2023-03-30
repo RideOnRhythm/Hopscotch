2eef2ab8a72d96a429b0b53bbbafd6660dad9ba3
RideOnRhythm <107351878+RideOnRhythm@users.noreply.github.com>
2023-03-30 20:17:04 +0800

    nagger

--- cogs/nft.py
+++ cogs/nft.py
@@ -34,6 +34,30 @@ class Nft(commands.Cog):
             async with aiohttp.ClientSession() as session:
                 async with session.get(url) as resp:
                     img = await resp.read()
+                    if resp.status == 403:
+                        continue
+                    with io.BytesIO(img) as file:
+                        files.append(discord.File(file, 'generated_nft.png'))
+        while len(files) < amount:
+            with open(
+                    os.path.dirname(__file__) + '/../assets/nft_list.txt',
+                    'r') as f:
+                urls = f.read().split('\n')
+                url = random.choice(urls)
+            while url in self.queue:
+                with open(
+                        os.path.dirname(__file__) + '/../assets/nft_list.txt',
+                        'r') as f:
+                    urls = f.read().split('\n')
+                    url = random.choice(urls)
+            self.queue.append(url)
+            if len(self.queue) >= 100:
+                self.queue = []
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as resp:
+                    img = await resp.read()
+                    if resp.status == 403:
+                        continue
                     with io.BytesIO(img) as file:
                         files.append(discord.File(file, 'generated_nft.png'))
         await ctx.send(files=files)
